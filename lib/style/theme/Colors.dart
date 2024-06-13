0bc17ad859700564d35dbc8af26b5ec3d1909571
useonguk <ysu0219@gmail.com>
2024-06-13 11:19:11 +0900

    FIX: primary color

--- lib/style/theme/Colors.dart
+++ lib/style/theme/Colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ThemeColors {
-  static const Color primary = Color(0x85E657);
+  static const Color primary = Color(0xFF85E657);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
 }
