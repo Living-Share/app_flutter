79a9d2b95dfb1846ab2ba5d04ccbcd2984ba18f7
유성욱 <yoo-seonguk@yuseong-ug-ui-noteubug.local>
2024-06-13 09:16:12 +0900

    SET: theme style

---
+++ lib/style/theme/Colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color primary = Color(0xD9D9D9);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+}
---
+++ lib/style/theme/Text.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DevCoopTextStyle {
+  static const bold_50 = TextStyle(
+    fontFamily: "GmarketSansB",
+    fontStyle: FontStyle.normal,
+    fontSize: 50.0,
+  );
+}
