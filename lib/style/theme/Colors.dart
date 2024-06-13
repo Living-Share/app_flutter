47b1d5f1f7b1d6054cbbd7a192532e7660e1f1ec
유성욱 <yoo-seonguk@yuseong-ug-ui-noteubug.local>
2024-06-13 09:25:29 +0900

    ADD: primary color 수정

--- lib/style/theme/Colors.dart
+++ lib/style/theme/Colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Colors {
-  static const Color primary = Color(0xD9D9D9);
+  static const Color primary = Color(0x85E657);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
 }
