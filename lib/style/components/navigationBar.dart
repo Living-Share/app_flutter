d93f8dc06a44ccd4e32b40e341830933a4c23afd
useonguk <ysu0219@gmail.com>
2024-06-22 23:13:24 +0900

    SET: bootom navigate bar

--- lib/style/components/navigationBar.dart
+++ lib/style/components/navigationBar.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 
 BottomNavigationBar BottomBar() {
   return BottomNavigationBar(
+    selectedItemColor: Colors.black, // 선택된 아이템 색상
+    unselectedItemColor: Colors.black, // 선택되지 않은 아이템 색상
     items: const <BottomNavigationBarItem>[
       BottomNavigationBarItem(
         icon: Icon(Icons.home),
-        label: 'Home',
+        label: "",
       ),
       BottomNavigationBarItem(
-        icon: Icon(Icons.search),
-        label: 'Search',
+        icon: Icon(Icons.calendar_today),
+        label: "",
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.attach_money),
+        label: "",
       ),
       BottomNavigationBarItem(
         icon: Icon(Icons.person),
-        label: 'Profile',
+        label: "",
       ),
     ],
   );
